a1673b4c67d3ac3d5d750071715b36b057472f4b
shaokeyibb@qq.com <shaokeyibb@qq.com>
2020-08-06 20:21:59 +0800

    There is no update (

--- PoHai (Version 2.0).py
+++ PoHai (Version 2.0).py
@@ -10,35 +10,35 @@ import win32con
 # while 判断 及 窗口。
 count = 3
 while count >= 0:
-    if Language == 'zh-CN':
-        letter = input("Please type the letter")
+    if Language == 'zh-cmn-Hans':
+        letter = input("Please type the letter : ")
         if letter == 'xzsx':
             print("恭喜你!你成功输入了密码!")
             print("xzsx的意思是:小猪是屑!(拼音输入法)")
             win32api.MessageBox(None,"小猪屑！","WTP016 迫害窗口",win32con.MB_OK)
-            break
+            exit("See you again~")
         else:
             print("你输入的数字错误!请再次输入!")
             count = 3 - 1
 
-    if Language == 'zh-TW':
-        letter = input("Please type the letter")
+    if Language == 'zh-cmn-Hant':
+        letter = input("Please type the letter : ")
         if letter == 'xzsx':
             print("恭喜你!你成功輸入了密碼!")
             print("xzsx的意思是:小豬是屑!(中國大陸拼音输入法)")
             win32api.MessageBox(None,"小猪是屑!","WTP016 的 迫害窗口",win32con.MB_OK)
-            break
+            exit("See you again!")
         else:
             print("你輸入的数字錯誤!請重新輸入!")
             count = 3 - 1
 
-    if Language == 'en-UK':
-        letter = input("Please type the letter")
+    if Language == 'en':
+        letter = input("Please type the letter : ")
         if letter == 'xzsx':
             print("Congratulations!You type letter is right!")
             print("The letter 'xzsx' mean:xiaozhu_zty is xie!")
             win32api.MessageBox(None,"xiaozhu_zty is xie!","WTP016's PoHai windows",win32con.MB_OK)
-            break
+            exit("Goodbye!")
         else:
             print("The letter you entered is incorrect! Please enter again!")
             count = 3 - 1
@@ -46,5 +46,4 @@ while count >= 0:
 # 此版本使用了 win32api 和 win32con 模块 并 加入了 次数统计 (while True , count 与 break)
 # 即将加入:
 # speech模块
-# 此版本已知BUG: 语言文本未更改。
 
