b7ba396422aa05ead7e5c4ca9bc39567951e489c
shaokeyibb@qq.com <shaokeyibb@qq.com>
2020-08-12 17:08:50 +0800

    # Commit Message **Update PoHai (Version 2.1).py, Add shaokeyibb PoHai v2.0.py**

--- PoHai (Version 2.1).py
+++ PoHai (Version 2.1).py
@@ -1,16 +1,21 @@
 # 语言选择
 print("The recognition of this script is strictly case sensitive, please enter it correctly！")
 print("该脚本严格区分大小写，请正确输入'zh-cmn-Hans'等语言代码")
-Language = input("请问你使用什么语言?如果你使用中文(简体)输入zh-cmn-Hans，使用中文(繁体)输入zh-cmn-Hant，使用英语输入en。")
+print(''' 
+[zh-cmn-Hans] 简体中文 (中国大陆)
+[zh-cmn-Hant] 繁體中文 (中國台灣,中國香港,中國澳門)
+[en] English (United States, United Kingdom and so on)''')
+Language = input("请在此输入上方 [] 内 的字符 : ")
 
 # 模块导入
 import win32api
+import tkinter
 import win32con
 import speech
 
 # while 判断 及 窗口。
 count = 3
-while count >= 0:
+while count > 0:
     if Language == 'zh-cmn-Hans':
         letter = input("Please type the letter : ")
         if letter == 'xzsx':
@@ -18,9 +23,9 @@ while count >= 0:
             print("xzsx的意思是:小猪是屑!(拼音输入法)")
             win32api.MessageBox(None,"小猪屑！","WTP016 迫害窗口",win32con.MB_OK)
             speech.say("小猪太屑了!")
-            exit("See you again~")
+            exit("希望还能再见!")
         else:
-            print("你输入的数字错误!请再次输入!")
+            print("你输入的字母错误!请再次输入!")
             count = 3 - 1
 
     if Language == 'zh-cmn-Hant':
@@ -32,7 +37,7 @@ while count >= 0:
             speech.say("小猪太屑了!")
             exit("See you again!")
         else:
-            print("你輸入的数字錯誤!請重新輸入!")
+            print("你輸入的字母錯誤!請重新輸入!")
             count = 3 - 1
 
     if Language == 'en':
